f84ac6d2cd065ca545e0b59a8fd70d20131de9e3
Jason Lobell <81822609+Jayman391@users.noreply.github.com>
2024-03-17 16:56:52 -0400

    session and optimization templates initialized (#1)
    Co-authored-by: Jason Lobell <jasonlobell@vermonster.com>

---
+++ bertcli/util/_session.py
@@ -0,0 +1,9 @@
+
+class Session:
+
+  def __init__(self, data=None, config_topic_model=None, config_optimization=None) -> None:
+    self.data = data
+    self.config_topic_model = config_topic_model
+    self.config_optimization = config_optimization
+    self.logs = {}
+    
