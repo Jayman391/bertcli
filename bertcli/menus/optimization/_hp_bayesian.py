4a80a4363f93d48e55284c4e246aa870aa23c555
Jason Lobell <jasonlobell@vermonster.com>
2024-03-17 22:17:49 -0400

    more cleanup, skeletons of grid and bayesian hp menus

--- bertcli/menus/optimization/_hp_bayesian.py
+++ bertcli/menus/optimization/_hp_bayesian.py
@@ -5,13 +5,6 @@ class BayesianHyperParameterMenu(Menu):
     is_root = False
     is_leaf = True
     options = [
-      "Choose a section to optimize",
-      "Choose a scoring function",
-      "Enable Bayesian Optimization",
-      "Choose a heuristic for optimization",
-      "Configure Hyperparameter Distributions",
-      "Configure Plotting",
-      "Run Optimization",
     ]
 
     super().__init__(options, is_leaf, is_root)
