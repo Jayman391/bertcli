922a9f74bfd4e6c8050e2bbae2b063275279a813
Jason Lobell <jasonlobell@vermonster.com>
2024-03-18 13:51:50 -0400

    topic model factory, testing for factory and session object

--- src/util/_session.py
+++ src/util/_session.py
@@ -1,3 +1,5 @@
+from src.builders._tm_factory import TopicModelFactory
+
 class Session:
 
   def __init__(self, data=None, config_topic_model=None, config_optimization=None) -> None:
@@ -10,3 +12,21 @@ class Session:
       "errors": []
     }
     self.plot_dir = None
+
+    self.topic_model_factory = TopicModelFactory()
+
+  def set_data(self, data):
+    self.data = data
+    return self.data
+  
+  def set_config_topic_model(self, config):
+    self.config_topic_model = config
+    return self.config_topic_model
+  
+  def set_config_optimization(self, config):
+    self.config_optimization = config
+    return self.config_optimization
+
+  def initialize_topic_model_factory(self):
+    self.topic_model_factory.upload_data(self.data)
+    return self.topic_model_factory
